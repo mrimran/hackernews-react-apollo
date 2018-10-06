6408878db213ff976985f9d8f7fd85e6c3cb6670
imran <mrimranzahoor@gmail.com>
2018-10-07 01:33:56 +0500

    Added Link, LinkList and AddLink components + the first setup of the application with successful calls to GraphQL server.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import LinkList from './LinkList'
+import CreateLink from './AddLink'
+
+
+
+class App extends Component {
+  render() {
+    return <CreateLink />
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './styles/index.css';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from './serviceWorker';
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context';
+import {AUTH_TOKEN} from './components/constants'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})
+
+const authLink = setContext((_, {headers}) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : 
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjam11eWk2MWM0YXljMGIyODY2NHkxOW9jIiwiaWF0IjoxNTM4NjgwMjY5fQ.Ask3Uf-J6nYK-yUcV5B7Yzo9hh3YY3a1NLthfaLMY9I'
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ serviceWorker.unregister();
